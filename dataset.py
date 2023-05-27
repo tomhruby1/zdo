fa32e505bd33001e2ba22c9415c29431b21a5886
tomas <tom.hruby1@gmail.com>
2023-05-27 19:30:13 +0200

    ahhhhhhh

--- dataset.py
+++ dataset.py
@@ -23,6 +23,8 @@ STD = np.array([47.85307378, 43.52442252, 44.18917168])
 
 MAX_STITCHES = 16
 
+IGNORE = ['128', '180', '56']
+
 class ZdoDataset(Dataset): 
     def __init__(self, data_json_p, images_p=Path('data/images/default'), 
                  image_size=(128, 255), transform=None, normalize=True, 
@@ -38,7 +40,7 @@ class ZdoDataset(Dataset):
         self.images_p = images_p
         self.transform = transform
         # only couple small images - all will be loaded into memory
-        self.data, self.images = interpolate_to_size(data, size=image_size)
+        self.data, self.images = interpolate_to_size(data, size=image_size, ignore_ids=IGNORE)
         self.normalize = normalize
         # in x,y shape ... h,w != x,y
         self.image_size = np.asarray([image_size[1], image_size[0]], dtype=float) 
@@ -58,19 +60,39 @@ class ZdoDataset(Dataset):
             stitches_points = np.array(self.data[image_id]['stitches'], dtype=float)
             
             stitches_labels = np.zeros(MAX_STITCHES)
-             # init with random vals around the center
+            # init with somewhat random vals around the center
             stitches_points2 = np.random.rand(MAX_STITCHES, 2, 2) * 10
             N = len(stitches_points)
             if N > 0:
                 for i in range(MAX_STITCHES):
                     stitches_points2[i] += np.array([[12*(i+1), 30], [12*(i+1), 80]])
             
-            # distribute N stitches randomly through the final fixed sized stitches array
+            # distribute N stitches through the final fixed sized stitches array
             if N > 0:
-                indices = np.arange(MAX_STITCHES)
-                np.random.shuffle(indices)
-                stitches_points2[indices[:N], :] = stitches_points
-                stitches_labels[indices[:N]] = 1
+                # if done randomly:
+                # indices = np.arange(MAX_STITCHES)
+                # np.random.shuffle(indices)
+                # stitches_points2[indices[:N], :] = stitches_points
+                # stitches_labels[indices[:N]] = 1
+                # for i in range(MAX_STITCHES):
+                #     stitch_reg_idx = 
+                reg = np.array(stitches_points2)
+                stitches_labels = np.zeros(MAX_STITCHES)
+                # assign each stitch to the closes available reg
+                for stitch_idx in range(N):
+                    dist2regs = [99999]*MAX_STITCHES
+                    # sort stitch regressors for each stitch by distance
+                    for j in range(MAX_STITCHES):
+                        dist2regs[j] = np.linalg.norm(stitches_points[stitch_idx] - reg[j])
+                    closest_regs = np.argsort(dist2regs)
+
+                    for r_id in closest_regs:
+                        if stitches_labels[r_id] != 0:
+                            continue
+                        else: 
+                            stitches_labels[r_id] = 1
+                            stitches_points2[r_id, :] = stitches_points[stitch_idx]
+                            break
 
             stitches_points2 = stitches_points2.astype('float32')
             stitches_points2 = stitches_points2.reshape([MAX_STITCHES*2, 2])
@@ -84,6 +106,11 @@ class ZdoDataset(Dataset):
     
     def __getitem__(self, idx):
         image_id = list(self.data.keys())[idx]
+        im, points, stitch_labels = self.get_item_by_id(image_id)
+        
+        return im, points, stitch_labels
+        
+    def get_item_by_id(self,image_id):
         im = self.images[image_id]
         points = self.points[image_id]
         if self.transform is not None:
@@ -118,13 +145,17 @@ class ZdoDataset(Dataset):
         
         return im, np.array(points)
 
-def interpolate_to_size(data, size=(128,255)):
+def interpolate_to_size(data, size=(128,255), ignore_ids=None):
     '''Interpolates the image data to the provided size. 
         Annotations are transformed accordingly.'''
     images_interpolated = {} #interpolated image tensors
     data_new = copy.deepcopy(data)
     for image_id in data:
-
+        if ignore_ids is not None:
+            if image_id in ignore_ids:
+                data_new.pop(image_id)
+                continue
+        
         image_p = Path('data/images/default')/data[str(image_id)]['file']
 
         im = read_image(str(image_p))
@@ -156,7 +187,7 @@ def interpolate_to_size(data, size=(128,255)):
     return data_new, images_interpolated
 
 def visualize(image:Union[torch.Tensor, np.ndarray], incision:Union[torch.Tensor, np.ndarray],
-              stitches:Union[torch.Tensor, np.ndarray] = None, stich_objectness = None,
+              stitches:Union[torch.Tensor, np.ndarray] = None, stitch_objectness = None,
               show_points=True, unnormalize=False, imsize=(128, 255)):
     if type(image) == torch.Tensor:
         image = image.permute(1,2,0).numpy()
@@ -183,16 +214,22 @@ def visualize(image:Union[torch.Tensor, np.ndarray], incision:Union[torch.Tensor
             ax.plot(x_coords[i], y_coords[i],'xc')
 
     # plot stitches
-    if type(stitches) == torch.Tensor:    
-        stitches = stitches.cpu().numpy()
-    for i in range(0, len(stitches), 2):
-        x_coords, y_coords = zip(*stitches[i:i+2,:])
-        x_coords = np.array(x_coords, dtype=float) 
-        y_coords = np.array(y_coords, dtype=float)
-        if unnormalize:
-            x_coords = (x_coords+1) * float(imsize[1])/2  
-            y_coords = (y_coords+1) * float(imsize[0])/2
-        ax.plot(x_coords, y_coords, 'go-')
+    if stitches is not None:
+        if type(stitches) == torch.Tensor:    
+            stitches = stitches.cpu().numpy()
+        stitch_count = 0
+        for i in range(0, len(stitches), 2):
+            x_coords, y_coords = zip(*stitches[i:i+2,:])
+            x_coords = np.array(x_coords, dtype=float) 
+            y_coords = np.array(y_coords, dtype=float)
+            alpha = 1.0
+            if stitch_objectness is not None:
+                alpha = float(stitch_objectness[stitch_count])
+            stitch_count += 1
+            if unnormalize:
+                x_coords = (x_coords+1) * float(imsize[1])/2  
+                y_coords = (y_coords+1) * float(imsize[0])/2
+            ax.plot(x_coords, y_coords, color=COLOR['Stitch'], marker='o', alpha=alpha)
     plt.show()
 
 def visualize_data(data, image_id, image=None, incision=None):
---
+++ torch_incision_model.py
@@ -0,0 +1,60 @@
+# import time
+
+# import torch.nn.functional as F
+# from torchvision.io import read_image
+# from torch.utils.data import Dataset, DataLoader, random_split
+# import torch.optim as optim
+# import albumentations as A
+
+import torch
+import torch.nn as nn
+from torchvision.models import vgg11, VGG11_Weights
+from torchvision.models import resnet18, ResNet18_Weights
+
+# from dataset import *
+
+backbone_out = 512*4*7
+# define model
+class Model1(nn.Module):
+    def __init__(self, pretrained_model):
+        super().__init__()
+        self.pretrained = pretrained_model
+        self.fc1 = nn.Linear(backbone_out, 512) # intermediate layer between predictons
+        self.fc_points_out = nn.Linear(512, 32) # 96 coordinates for 16 incisions + 16 stitches
+        self.dropout = nn.Dropout(0.4)
+    
+    def forward(self, x):
+        x = nn.functional.relu(self.pretrained(x)) # shape (512, 4, 7) here (for (3,128,255))
+        x = torch.flatten(x, start_dim=1)
+        x = nn.functional.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = torch.tanh(self.fc_points_out(x))
+        return x
+
+
+def load(path):
+    '''loads .pth checkpoint file given its path'''
+    checkpoint = torch.load(path)
+    backbone = vgg11()
+    backbone = list(backbone.children())[0]
+    model_loaded = Model1(backbone)
+    model_loaded.load_state_dict(checkpoint['model_state_dict'])
+    model_loaded.eval()
+    model_loaded.to('cpu')
+
+    return model_loaded
+
+
+def run_inference(model, image):
+    ''' In: image of size 128x255 
+        out: 32 incision points coordinates '''
+    model.eval()
+    im = (image/128.0) - 1
+    im2 = (im.unsqueeze(0)).to('cpu')
+    points_pred = model(im2)
+    points_pred = points_pred.reshape([16,2]).detach().cpu()
+    points_pred += 1
+    points_pred[:,0] *= (255.0 /2)
+    points_pred[:,1] *= (128.0 /2)
+    return points_pred.numpy()
+
